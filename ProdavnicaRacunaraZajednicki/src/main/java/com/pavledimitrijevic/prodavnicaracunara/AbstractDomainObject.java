8812ff938e687ec76362ff64782773d552dc5d5c
Pavle Dimitrijevic <dimitrijevicc.p@gmail.com>
2026-04-13 20:24:07 +0200

    Dodate klase AbstractDomainObject i Administrator

---
+++ ProdavnicaRacunaraZajednicki/src/main/java/com/pavledimitrijevic/prodavnicaracunara/AbstractDomainObject.java
@@ -0,0 +1,31 @@
+package com.pavledimitrijevic.prodavnicaracunara;
+
+import java.io.Serializable;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.util.ArrayList;
+
+/**
+ *
+ * @author PAVLE
+ */
+public abstract class AbstractDomainObject implements Serializable {
+
+    public abstract String nazivTabele();
+
+    public abstract String alijas();
+
+    public abstract String join();
+
+    public abstract ArrayList<AbstractDomainObject> vratiListu(ResultSet rs) throws SQLException;
+
+    public abstract String koloneZaInsert();
+
+    public abstract String uslov();
+
+    public abstract String vrednostiZaInsert();
+
+    public abstract String vrednostiZaUpdate();
+
+    public abstract String uslovZaSelect();
+}
