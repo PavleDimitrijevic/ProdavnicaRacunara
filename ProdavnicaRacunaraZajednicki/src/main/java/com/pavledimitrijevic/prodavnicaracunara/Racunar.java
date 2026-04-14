4ed2e90687014f74f374d115410e5bc337391461
Pavle Dimitrijevic <dimitrijevicc.p@gmail.com>
2026-04-14 14:06:38 +0200

    Dokumentovana klasa Racunar

--- ProdavnicaRacunaraZajednicki/src/main/java/com/pavledimitrijevic/prodavnicaracunara/Racunar.java
+++ ProdavnicaRacunaraZajednicki/src/main/java/com/pavledimitrijevic/prodavnicaracunara/Racunar.java
@@ -5,27 +5,80 @@ import java.sql.SQLException;
 import java.util.ArrayList;
 
 /**
+ * Predstavlja racunar koja se prodaje u prodavnici.
+ * <p>
+ * Svaki racunar ima jedinstveni ID, naziv, cenu po komadu, opis, tip racunara,
+ * listu komponenti i garanaciju.
+ * </p>
  *
  * @author PAVLE
  */
 public class Racunar extends AbstractDomainObject {
 
+    /**
+     * ID racunara kao Long
+     */
     private Long racunarID;
+
+    /**
+     * ID racunara kao Long
+     */
     private String naziv;
+
+    /**
+     * Cena po komadu racunara kao double
+     */
     private double cenaPoKomadu;
+
+    /**
+     * Opis racunara kao String
+     */
     private String opis;
+
+    /**
+     * Tip racunara
+     */
     private TipRacunara tipRacunara;
+
+    /**
+     * Lista komponenti racunara
+     */
     private ArrayList<Komponenta> komponente;
+
+    /**
+     * Garancija na racunar
+     */
     private Garancija garancija;
 
+    /**
+     * Vraca tekstualni prikaz racunara.
+     *
+     * @return String sa podacima o nazivu i ceni racunara u formatu naziv + "
+     * (Cena po komadu: " + cenaPoKomadu + "din)"
+     */
     @Override
     public String toString() {
         return naziv + " (Cena po komadu: " + cenaPoKomadu + "din)";
     }
 
+    /**
+     * Inicijalizuje objekat klase Racunar sa atributima koji imaju default
+     * vrednosti.
+     */
     public Racunar() {
     }
 
+    /**
+     * Inicijalizuje objekat klase Racunar sa parametrima.
+     *
+     * @param racunarID Jedinstveni ID racunara. Mora biti pozitivan broj i ne
+     * sme biti null.
+     * @param naziv Naziv racunara. Ne sme biti null ili prazan string.
+     * @param cenaPoKomadu Cena jednoog racunara. Mora biti veca od nule.
+     * @param opis Opis racunara. Ne sme biti null niti prazan string.
+     * @param tipRacunara Tip racunara. Ne sme biti null.
+     * @param komponente Lista komponenti. Ne sme biti null niti prazna.
+     */
     public Racunar(Long racunarID, String naziv, double cenaPoKomadu, String opis, TipRacunara tipRacunara, ArrayList<Komponenta> komponente) {
         setRacunarID(racunarID);
         setNaziv(naziv);
@@ -35,6 +88,18 @@ public class Racunar extends AbstractDomainObject {
         setKomponente(komponente);
     }
 
+    /**
+     * Inicijalizuje objekat klase Racunar sa svim parametrima.
+     *
+     * @param racunarID Jedinstveni ID racunara. Mora biti pozitivan broj i ne
+     * sme biti null.
+     * @param naziv Naziv racunara. Ne sme biti null ili prazan string.
+     * @param cenaPoKomadu Cena jednoog racunara. Mora biti veca od nule.
+     * @param opis Opis racunara. Ne sme biti null niti prazan string.
+     * @param tipRacunara Tip racunara. Ne sme biti null.
+     * @param komponente Lista komponenti. Ne sme biti null niti prazna.
+     * @param garancija Garancija na racunar
+     */
     public Racunar(Long racunarID, String naziv, double cenaPoKomadu, String opis,
             TipRacunara tipRacunara, ArrayList<Komponenta> komponente,
             Garancija garancija) {
@@ -47,10 +112,23 @@ public class Racunar extends AbstractDomainObject {
         setGarancija(garancija);
     }
 
+    /**
+     * Vraca listu komponenti racunara.
+     *
+     * @return lista komponenti
+     */
     public ArrayList<Komponenta> getKomponente() {
         return komponente;
     }
 
+    /**
+     * Postavlja listu komponenti. Lista komponenti ne sme biti null niti
+     * prazna.
+     *
+     * @param komponente Lista komponenti
+     * @throws java.lang.NullPointerException ako je lista null
+     * @throws java.lang.IllegalArgumentException ako je lista prazna
+     */
     public void setKomponente(ArrayList<Komponenta> komponente) {
         if (komponente == null) {
             throw new NullPointerException("Lista komponenti ne sme biti null.");
@@ -62,10 +140,24 @@ public class Racunar extends AbstractDomainObject {
         this.komponente = komponente;
     }
 
+    /**
+     * Vraca ID racunara.
+     *
+     * @return ID racunara
+     */
     public Long getRacunarID() {
         return racunarID;
     }
 
+    /**
+     * Postavlja ID racunara. ID racunara mora biti pozitivan broj i ne sme biti
+     * null.
+     *
+     * @param racunarID ID racunara
+     * @throws java.lang.NullPointerException ako je ID null
+     * @throws java.lang.IllegalArgumentException ako je ID manji ili jednak
+     * nuli
+     */
     public void setRacunarID(Long racunarID) {
         if (racunarID == null) {
             throw new NullPointerException("ID ne sme biti null.");
@@ -78,10 +170,22 @@ public class Racunar extends AbstractDomainObject {
         this.racunarID = racunarID;
     }
 
+    /**
+     * Vraca naziv racunara.
+     *
+     * @return naziv racunara
+     */
     public String getNaziv() {
         return naziv;
     }
 
+    /**
+     * Postavlja naziv racunara. Naziv ne sme biti null niti prazan string.
+     *
+     * @param naziv Naziv racunara
+     * @throws java.lang.NullPointerException ako je naziv null
+     * @throws java.lang.IllegalArgumentException ako je naziv prazan string
+     */
     public void setNaziv(String naziv) {
         if (naziv == null) {
             throw new NullPointerException("Naziv ne sme biti null.");
@@ -94,10 +198,22 @@ public class Racunar extends AbstractDomainObject {
         this.naziv = naziv;
     }
 
+    /**
+     * Vraca cenu jednog racunara.
+     *
+     * @return cena po komadu
+     */
     public double getCenaPoKomadu() {
         return cenaPoKomadu;
     }
 
+    /**
+     * Postavlja cenu jednog racunara. Cena mora biti veca od nule.
+     *
+     * @param cenaPoKomadu Cena racunara
+     * @throws java.lang.IllegalArgumentException ako je cena manja ili jednaka
+     * nuli
+     */
     public void setCenaPoKomadu(double cenaPoKomadu) {
         if (cenaPoKomadu <= 0) {
             throw new IllegalArgumentException("Cena po komadu mora biti veca od nule.");
@@ -105,10 +221,22 @@ public class Racunar extends AbstractDomainObject {
         this.cenaPoKomadu = cenaPoKomadu;
     }
 
+    /**
+     * Vraca opis racunara.
+     *
+     * @return opis racunara
+     */
     public String getOpis() {
         return opis;
     }
 
+    /**
+     * Postavlja opis racunara. Opis ne sme biti null niti prazan string.
+     *
+     * @param opis Opis racunara
+     * @throws java.lang.NullPointerException ako je opis null
+     * @throws java.lang.IllegalArgumentException ako je opis prazan string
+     */
     public void setOpis(String opis) {
         if (opis == null) {
             throw new NullPointerException("Opis ne sme biti null.");
@@ -120,10 +248,21 @@ public class Racunar extends AbstractDomainObject {
         this.opis = opis;
     }
 
+    /**
+     * Vraca tip racunara.
+     *
+     * @return tip racunara
+     */
     public TipRacunara getTipRacunara() {
         return tipRacunara;
     }
 
+    /**
+     * Postavlja tip racunara. Tip racunara ne sme biti null.
+     *
+     * @param tipRacunara Tip racunara
+     * @throws java.lang.NullPointerException ako je tip racunara null
+     */
     public void setTipRacunara(TipRacunara tipRacunara) {
         if (tipRacunara == null) {
             throw new NullPointerException("Tip racunara ne sme biti null.");
@@ -131,10 +270,21 @@ public class Racunar extends AbstractDomainObject {
         this.tipRacunara = tipRacunara;
     }
 
+    /**
+     * Vraca garanciju racunara.
+     *
+     * @return garancija
+     */
     public Garancija getGarancija() {
         return garancija;
     }
 
+    /**
+     * Postavlja garanciju racunara.Garancija racunara ne sme biti null.
+     *
+     * @param garancija Garancija racunara
+     * @throws java.lang.NullPointerException ako je garancija racunara null
+     */
     public void setGarancija(Garancija garancija) {
         if (garancija == null) {
             throw new NullPointerException("Garancija ne sme biti null.");
