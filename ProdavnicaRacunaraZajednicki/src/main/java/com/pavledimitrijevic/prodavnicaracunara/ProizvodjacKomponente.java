410f20293922617a893b65579da25eb3917bda3e
Pavle Dimitrijevic <dimitrijevicc.p@gmail.com>
2026-04-14 14:27:34 +0200

    Dokumentovana klasa ProizvodjacKomponente

--- ProdavnicaRacunaraZajednicki/src/main/java/com/pavledimitrijevic/prodavnicaracunara/ProizvodjacKomponente.java
+++ ProdavnicaRacunaraZajednicki/src/main/java/com/pavledimitrijevic/prodavnicaracunara/ProizvodjacKomponente.java
@@ -3,26 +3,61 @@ package com.pavledimitrijevic.prodavnicaracunara;
 import java.io.Serializable;
 
 /**
+ * Predstavlja proizvodjaca komponente racunara.
+ * <p>
+ * Svaki proizvodjac ima jedinstveni ID i naziv.
+ * </p>
  *
  * @author PAVLE
  */
 public class ProizvodjacKomponente implements Serializable {
 
+    /**
+     * ID proizvodjaca kao Long
+     */
     private Long proizvodjacID;
+
+    /**
+     * Naziv proizvodjaca kao String
+     */
     private String naziv;
 
+    /**
+     * Inicijalizuje objekat klase ProizvodjacKomponente sa atributima koji
+     * imaju default vrednosti.
+     */
     public ProizvodjacKomponente() {
     }
 
+    /**
+     * Inicijalizuje objekat klase ProizvodjacKomponente sa parametrima.
+     *
+     * @param proizvodjacID Jedinstveni ID proizvodjaca. Mora biti pozitivan
+     * broj i ne sme biti null.
+     * @param naziv Naziv proizvodjaca. Ne sme biti null niti prazan string.
+     */
     public ProizvodjacKomponente(Long proizvodjacID, String naziv) {
         setProizvodjacID(proizvodjacID);
         setNaziv(naziv);
     }
 
+    /**
+     * Vraca ID proizvodjaca.
+     *
+     * @return ID proizvodjaca
+     */
     public Long getProizvodjacID() {
         return proizvodjacID;
     }
 
+    /**
+     * Postavlja ID proizvodjaca. Mora biti pozitivan broj i ne sme biti null.
+     *
+     * @param proizvodjacID ID proizvodjaca
+     * @throws java.lang.NullPointerException ako je ID null
+     * @throws java.lang.IllegalArgumentException ako je ID manji ili jednak
+     * nuli
+     */
     public void setProizvodjacID(Long proizvodjacID) {
         if (proizvodjacID == null) {
             throw new NullPointerException("ID proizvodjaca ne sme biti null.");
@@ -35,10 +70,22 @@ public class ProizvodjacKomponente implements Serializable {
         this.proizvodjacID = proizvodjacID;
     }
 
+    /**
+     * Vraca naziv proizvodjaca.
+     *
+     * @return naziv proizvodjaca
+     */
     public String getNaziv() {
         return naziv;
     }
 
+    /**
+     * Postavlja naziv proizvodjaca. Ne sme biti null niti prazan string.
+     *
+     * @param naziv naziv proizvodjaca
+     * @throws java.lang.NullPointerException ako je naziv null
+     * @throws java.lang.IllegalArgumentException ako je naziv prazan string
+     */
     public void setNaziv(String naziv) {
         if (naziv == null) {
             throw new NullPointerException("Naziv proizvodjaca ne sme biti null.");
