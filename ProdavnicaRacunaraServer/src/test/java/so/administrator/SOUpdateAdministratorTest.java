17b1308f2c07eced942b7c19aba332d4926fa51e
Pavle Dimitrijevic <dimitrijevicc.p@gmail.com>
2026-04-19 14:23:11 +0200

    Testirana sistemska operacija UpdateAdministrator

--- ProdavnicaRacunaraServer/src/test/java/so/administrator/SOUpdateAdministratorTest.java
+++ ProdavnicaRacunaraServer/src/test/java/so/administrator/SOUpdateAdministratorTest.java
@@ -1,54 +1,106 @@
 package so.administrator;
 
 import com.pavledimitrijevic.prodavnicaracunara.AbstractDomainObject;
+import com.pavledimitrijevic.prodavnicaracunara.Administrator;
+import com.pavledimitrijevic.prodavnicaracunara.Racunar;
+import db.DBBroker;
+import java.sql.SQLException;
+import java.util.ArrayList;
 import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.AfterAll;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.Test;
 import static org.junit.jupiter.api.Assertions.*;
+import org.junit.jupiter.api.DisplayName;
 
 /**
  *
  * @author PAVLE
  */
 public class SOUpdateAdministratorTest {
-    
-    public SOUpdateAdministratorTest() {
-    }
-    
-    @BeforeAll
-    public static void setUpClass() {
-    }
-    
-    @AfterAll
-    public static void tearDownClass() {
-    }
-    
+
+    private SOUpdateAdministrator soUpdate;
+    private Administrator administrator;
+
     @BeforeEach
-    public void setUp() {
+    void setUp() {
+        soUpdate = new SOUpdateAdministrator();
+        administrator = new Administrator();
     }
-    
+
     @AfterEach
-    public void tearDown() {
+    void tearDown() {
+        soUpdate = null;
+        administrator = null;
     }
 
     @Test
-    public void testValidate() throws Exception {
-        System.out.println("validate");
-        AbstractDomainObject ado = null;
-        SOUpdateAdministrator instance = new SOUpdateAdministrator();
-        instance.validate(ado);
-        fail("The test case is a prototype.");
+    @DisplayName("Uspesno azuriranje administratora u bazi")
+    void testExecuteSuccessful() throws SQLException {
+        administrator.setIme("TestUpdate");
+        administrator.setPrezime("Execute");
+        administrator.setUsername("user" + System.currentTimeMillis());
+        administrator.setPassword("test12345");
+        DBBroker.getInstance().insert(administrator); // dodavanje testnog administratora u bazu
+
+        @SuppressWarnings("unchecked")
+        ArrayList<Administrator> administratori = (ArrayList<Administrator>) (ArrayList<?>) DBBroker.getInstance()
+                .select(administrator); // izvlacimo iz baze
+        Administrator ubacen = administratori.stream().filter(a -> a.getUsername().equals(administrator.getUsername()))
+                .findFirst().orElseThrow(() -> new RuntimeException("Administrator nije pronadjen nakon inserta!"));
+
+        String noviUsername = "updatedUser" + System.currentTimeMillis();
+        ubacen.setUsername(noviUsername);
+
+        assertDoesNotThrow(() -> soUpdate.validate(ubacen));
+        assertDoesNotThrow(() -> soUpdate.execute(ubacen));
+
+        @SuppressWarnings("unchecked")
+        ArrayList<Administrator> lista = (ArrayList<Administrator>) (ArrayList<?>) DBBroker.getInstance()
+                .select(new Administrator());
+        assertTrue(
+                lista.stream()
+                        .anyMatch(a -> a.getAdministratorID().equals(ubacen.getAdministratorID())
+                        && a.getUsername().equals(noviUsername)),
+                "Administrator nije azuriran sa novim username-om u bazi!");
+
+        DBBroker.getInstance().delete(administrator); // ciscenje baze da se ne bi prenatrpala podacima
+
     }
 
     @Test
-    public void testExecute() throws Exception {
-        System.out.println("execute");
-        AbstractDomainObject ado = null;
-        SOUpdateAdministrator instance = new SOUpdateAdministrator();
-        instance.execute(ado);
-        fail("The test case is a prototype.");
+    @DisplayName("Pogresan tip podataka")
+    void testValidationInvalidType() {
+        assertThrows(java.lang.Exception.class, () -> soUpdate.validate(new Racunar()));
     }
-    
+
+    @Test
+    @DisplayName("Username nije jedinstven u bazi")
+    void testValidationDuplicateUsername() throws SQLException {
+        String username = "user" + System.currentTimeMillis();
+
+        Administrator prvi = new Administrator();
+        prvi.setIme("ImePrvi");
+        prvi.setPrezime("PrezimePrvi");
+        prvi.setUsername(username);
+        prvi.setPassword("sifraPrvi123");
+        DBBroker.getInstance().insert(prvi);
+
+        Administrator duplikat = new Administrator();
+        duplikat.setIme("ImeDuplikat");
+        duplikat.setPrezime("PrezimeDuplikat");
+        duplikat.setUsername("UsernameDrugi" + System.currentTimeMillis());
+        duplikat.setPassword("sifraDrugi123");
+        DBBroker.getInstance().insert(duplikat);
+
+        duplikat.setUsername(prvi.getUsername()); // postavljanje username-a da bude kao za prvog administratora
+
+        assertThrows(java.lang.Exception.class, () -> soUpdate.validate(duplikat));
+
+        DBBroker.getInstance().delete(prvi);
+        DBBroker.getInstance().delete(duplikat);
+
+    }
+
 }
