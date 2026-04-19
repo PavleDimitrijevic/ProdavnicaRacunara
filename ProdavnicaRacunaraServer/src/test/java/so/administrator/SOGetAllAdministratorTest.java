ca536b973fc6bc308769ad0d98144a83ac0f6e0a
Pavle Dimitrijevic <dimitrijevicc.p@gmail.com>
2026-04-19 14:21:33 +0200

    Testirana sistemska operacija GetAllAdministrator

--- ProdavnicaRacunaraServer/src/test/java/so/administrator/SOGetAllAdministratorTest.java
+++ ProdavnicaRacunaraServer/src/test/java/so/administrator/SOGetAllAdministratorTest.java
@@ -1,66 +1,77 @@
 package so.administrator;
 
-import com.pavledimitrijevic.prodavnicaracunara.AbstractDomainObject;
 import com.pavledimitrijevic.prodavnicaracunara.Administrator;
+import com.pavledimitrijevic.prodavnicaracunara.Racunar;
+import db.DBBroker;
+import java.sql.SQLException;
 import java.util.ArrayList;
 import org.junit.jupiter.api.AfterEach;
-import org.junit.jupiter.api.AfterAll;
 import org.junit.jupiter.api.BeforeEach;
-import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.Test;
 import static org.junit.jupiter.api.Assertions.*;
+import org.junit.jupiter.api.DisplayName;
 
 /**
  *
  * @author PAVLE
  */
 public class SOGetAllAdministratorTest {
-    
-    public SOGetAllAdministratorTest() {
-    }
-    
-    @BeforeAll
-    public static void setUpClass() {
-    }
-    
-    @AfterAll
-    public static void tearDownClass() {
-    }
-    
+
+    private SOGetAllAdministrator soGetAll;
+    private Administrator administrator;
+
     @BeforeEach
-    public void setUp() {
+    void setUp() {
+        soGetAll = new SOGetAllAdministrator();
+        administrator = new Administrator();
     }
-    
+
     @AfterEach
-    public void tearDown() {
+    void tearDown() {
+        soGetAll = null;
+        administrator = null;
     }
 
     @Test
-    public void testValidate() throws Exception {
-        System.out.println("validate");
-        AbstractDomainObject ado = null;
-        SOGetAllAdministrator instance = new SOGetAllAdministrator();
-        instance.validate(ado);
-        fail("The test case is a prototype.");
+    @DisplayName("Uspesno vracanje liste svih administratora")
+    void testExecuteSuccessful() throws SQLException {
+        administrator.setIme("TestGetAll");
+        administrator.setPrezime("Execute");
+        administrator.setUsername("user" + System.currentTimeMillis());
+        administrator.setPassword("test12345");
+        DBBroker.getInstance().insert(administrator); // dodavanje testnog administratora u bazu
+
+        assertDoesNotThrow(() -> soGetAll.validate(administrator));
+        assertDoesNotThrow(() -> soGetAll.execute(administrator));
+
+        ArrayList<Administrator> lista = soGetAll.getLista();
+        assertNotNull(lista); // provera da je lista inicijalizovana
+        assertTrue(lista.stream().anyMatch(a -> a.getUsername().equals(administrator.getUsername()))); // u listi
+        // trazimo
+        // administratora
+        // kog smo
+        // dodali po
+        // username-u
+
+        DBBroker.getInstance().delete(administrator); // ciscenje baze da se ne bi bila prenatrpala podacima
+
     }
 
     @Test
-    public void testExecute() throws Exception {
-        System.out.println("execute");
-        AbstractDomainObject ado = null;
-        SOGetAllAdministrator instance = new SOGetAllAdministrator();
-        instance.execute(ado);
-        fail("The test case is a prototype.");
+    @DisplayName("Pogresan tip podataka")
+    void testValidationInvalidType() {
+        assertThrows(java.lang.Exception.class, () -> soGetAll.validate(new Racunar()));
+
     }
 
     @Test
-    public void testGetLista() {
-        System.out.println("getLista");
-        SOGetAllAdministrator instance = new SOGetAllAdministrator();
-        ArrayList<Administrator> expResult = null;
-        ArrayList<Administrator> result = instance.getLista();
-        assertEquals(expResult, result);
-        fail("The test case is a prototype.");
+    @DisplayName("Validacija ispravnog objekta")
+    void testValidationValidObject() {
+        administrator.setIme("Djorjde");
+        administrator.setPrezime("Djordjevic");
+        administrator.setUsername("valid" + System.currentTimeMillis());
+        administrator.setPassword("valid12345");
+
+        assertDoesNotThrow(() -> soGetAll.validate(administrator));
     }
-    
 }
